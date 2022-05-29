2c46fa3fcb09eba52b65a720bfb51622aa8ad52a
Dani.Am <Daniel.Amergouyev@gmail.com>
2022-05-29 17:09:30 +0300

    second mission pytest

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -9,7 +9,7 @@ from playwright.sync_api import Playwright, APIRequestContext
 
 @pytest.fixture(scope="session")
 def api_request_context(
-    playwright: Playwright,
+        playwright: Playwright,
 ) -> Generator[APIRequestContext, None, None]:
     headers = {
         # We set this header per GitHub guidelines.
@@ -26,41 +26,130 @@ def api_request_context(
     request_context.dispose()
 
 
+ws_id = '6b952c5d-7fd4-4a89-b9d0-e31f29338316'
 
 
+def test_create_workspace(api_request_context: APIRequestContext) -> None:
+    # Login #
 
-
-data_login = {
+    data_login = {
         "email": "hobeben160@angeleslid.com",
         "password": "123456",
         "org_id": "5edfd39d-d9cd-43dc-ab36-748a03aed01d"
     }
-
-
-data_workspace = {
-    "name": "dani",
-    "org_id": "",
-    "type": "recruiting",
-    "is_collaboration":False,
-    "members": [],
-    "topics": []
-    }
-
-
-
-
-def test_create_workspace(api_request_context:APIRequestContext) -> None:
 
     response = api_request_context.post(f"/api/v1/login", data=data_login)
 
     assert response.ok
 
     bearer = response.json()['access_token']
-    print('Bearer',bearer)
+    print('Bearer', bearer)
 
-    response = api_request_context.post("/api/v1/workspaces",headers={"Authorization": f"bearer {bearer}"}, data=data_workspace)
-    print('Response:',response.json())
+    # Create workspace #
 
-    assert response.status == '201'
+    data_workspace = {
+        "name": "dani",
+        "org_id": "5edfd39d-d9cd-43dc-ab36-748a03aed01d",
+        "type": "recruiting",
+        "is_collaboration": False,
+        "members": [],
+        "topics": []
+    }
+    ws_id = '6b952c5d-7fd4-4a89-b9d0-e31f29338316'
 
+    response = api_request_context.post(f"/api/v1/workspaces/", headers={"Authorization": f"bearer {bearer}"},
+                                        data=data_workspace)
+    print('Response:', response.json())
 
+    assert response.status == 201
+
+    # Create Topic #
+    data_topic = {
+
+        "name": "123456"
+    }
+
+    response = api_request_context.post(f"/api/v1/workspaces/{ws_id}/topics/",
+                                        headers={"Authorization": f"bearer {bearer}"}, data=data_topic)
+    topic_id = response.json()['id']
+    print('Respones topic:', response.json())
+
+    assert response.status == 201
+
+    # Create Skill #
+
+    data_skill = {
+        "name": "skill",
+        "workspace_id": f'{ws_id}',
+        "topic_id": f"{topic_id}",
+
+    }
+
+    response = api_request_context.post(f"api/v1/skills/", headers={
+        "Authorization": f"bearer {bearer}",
+        # "Accept": "*/*",
+        # "Accept-Encoding": "gzip, deflate",
+        # "Content-Length": "0",
+        # "Content-Type": "text/html; charset=UTF-8",
+    }, data=data_skill)
+
+    skill_id = response.json()['id']
+    print('Respones skill:', response.json())
+
+    assert response.status == 201
+
+    # Create Step #
+    data_step = {
+        "name": "string",
+        "display_mode": "progress",
+        "widgets": [
+            {
+                "widget_type": 1,
+                "description": "",
+                "is_required": False,
+                "is_gradeable": False,
+                "content": {
+                    "source": 101,
+                    "type": 1,
+                    "data": "string",
+                    "data_id": "6d9132b6-7d4b-48aa-b5c7-f341d7b86bfb",
+                    "description": "",
+                    "options": {
+                        "start_at_timestamp": "00:00",
+                        "media_duration": 0
+                    }
+                }
+            }
+        ],
+        "is_shared": False
+    }
+
+    response = api_request_context.post(f"/api/v1/skills/{skill_id}/steps/",
+                                        headers={"Authorization": f"bearer {bearer}"}, data=data_step)
+    step_id = response.json()["id"]
+    print('Respones step :', response.json())
+    assert response.status == 201
+
+    # Create widget
+    data_widget = {
+        "widget_type": 1,
+        "description": "",
+        "is_required": False,
+        "is_gradeable": False,
+        "content": {
+            "source": 101,
+            "type": 1,
+            "data": "string",
+            "data_id": "6d9132b6-7d4b-48aa-b5c7-f341d7b86bfb",
+            "description": "",
+            "options": {
+                "start_at_timestamp": "00:00",
+                "media_duration": 0
+            }
+        }
+    }
+
+    response = api_request_context.post(f"/api/v1/skills/{skill_id}/steps/{step_id}/widgets/",
+                                        headers={"Authorization": f"bearer {bearer}"}, data=data_widget)
+    print("Respones widget :", response.json())
+    assert response.status == 201
